bf2fa2c5e167e7b0d94cb56bee80091487a2afe7
kevin-england <kevinengland5051@gmail.com>
2016-01-28 10:05:28 -0800

    Received API and updated api.js file and also created a new JSON file for API response body, #1

--- api.js
+++ api.js
@@ -7,7 +7,8 @@ var textParser = bodyParser.text();
 //Get Angular connected with this info somehow
 
 router.get('/', function(req, res) {
-  request('https://api.toornament.com/v1/tournaments', function (error, response, body) {
+  var keyword = req.body;
+  request('https://na.api.pvp.net/observer-mode/rest/featured?api_key=0532b884-44d9-41f2-8b2f-a8b0b30a7e78', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var tournaments = JSON.parse(body);
       res.json(tournaments);
