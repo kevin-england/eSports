b7d485bd58f39d74e6fe2d6c04bc8918b6407cd1
kevin-england <kevinengland5051@gmail.com>
2016-01-29 14:00:08 -0800

    Using angular to take specific API object properties to be displayed in browser rather than displaying whole object, #2

--- public/js/angular.js
+++ public/js/angular.js
@@ -2,9 +2,9 @@ var app = angular.module('share', []);
 
 app.controller('mainController', function($http) {
   var vm = this;
-  vm.results = 'Search Results';
+  vm.results = 'Search Results:';
   $http({
      method: 'GET',
      url: '/featured'
-  }).success(function(data) { vm.games = data });
+  }).success(function(data) { vm.games = data; console.log(data) });
 });
