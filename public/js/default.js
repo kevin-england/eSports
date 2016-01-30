412cd19a29cc57238354d07b06ce2840bc97e449
kevin-england <kevinengland5051@gmail.com>
2016-01-29 16:01:16 -0800

    File clean up - getting rid of excess code in several files

--- public/js/default.js
+++ public/js/default.js
@@ -3,6 +3,16 @@ function hideContent(){
   content.style.display = 'none';
 }
 
+function featuredContent(){
+  var click = document.getElementById('featured');
+  click.addEventListener('click', function(){
+    var remove = document.getElementById('sponsors');    
+    remove.style.display = 'none';
+    var content = document.getElementById('content');    
+    content.style.display = 'block';
+  }, false);
+}
+
 function showContent(){
   var click = document.getElementById('submit');
   click.addEventListener('click', function(){
@@ -13,19 +23,6 @@ function showContent(){
   }, false);
 }
 
-function featuredCall() {
-  var getGame = new XMLHttpRequest();
-  getGame.onload = function() {
-    var routerResponse = document.getElementById('submit');
-    routerResponse.addEventListener('click', function(){
-      response = JSON.parse(getGame.responseText);
-    }, false)
-  }
-  getGame.open('GET', 'http://localhost:3000/featured', true);
-  getGame.send();
-}
-
-
 hideContent()
+featuredContent()
 showContent()
-featuredCall()
