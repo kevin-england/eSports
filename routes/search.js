ed154111bda572efb43556ec6a1820f7cb8f6963
kevin-england <kevinengland5051@gmail.com>
2016-02-01 11:29:41 -0800

    More stylization changes for API objects being received in search, #2

--- routes/search.js
+++ routes/search.js
@@ -4,7 +4,7 @@ var request = require('request');
 var bodyParser = require('body-parser');
 
 search.get('/', function(req, res) {
-  request('https://api.toornament.com/v1/tournaments{}?api_key=ERsQmUvqo_G8J-PbiGuUkKEsnGsg5P9roJ6fB7aCsoE', function (error, response, body) {
+  request('https://api.toornament.com/v1/tournaments{55ece437140ba09c4f8b4614}?api_key=ERsQmUvqo_G8J-PbiGuUkKEsnGsg5P9roJ6fB7aCsoE', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var games = JSON.parse(body);
       res.json(games);
