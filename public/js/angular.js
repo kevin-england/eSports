0f30e82389ddb8baa6c9bd22005fe944e051f19a
kevin-england <kevinengland5051@gmail.com>
2016-02-04 14:19:07 -0800

    Code clean up

--- public/js/angular.js
+++ public/js/angular.js
@@ -3,14 +3,6 @@ var app = angular.module('share', []);
 app.controller('mainController', function($http) {
   var vm = this;
   vm.results = 'Search Results:';
-  /* vm.update = function($http) {
-  method: 'GET',
-  url: '/featured'
-  }).success(function(data) { vm.games = data.streams });
-});
-  games.update().then(function(response) {
-    vm.show = response.data;
-  });*/
   vm.update = function() {
     console.log('update')
     $http({
